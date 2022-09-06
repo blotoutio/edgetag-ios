87eab3f2a8f6d3e6fd18933c69c975b09215785c
Poonam Tiwari <poonamt3489@gmail.com>
2022-09-06 12:50:57 +0530

    Removed unused code

--- EdgeTagSDKTests/EdgeTagSDKTests.swift
+++ EdgeTagSDKTests/EdgeTagSDKTests.swift
@@ -9,35 +9,12 @@ import XCTest
 @testable import EdgeTagSDK
 
 class EdgeTagSDKTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
     
     func testInitWithIDFADisabledAndConsentCheckEnabled(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: false,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(success)
         })
@@ -46,7 +23,7 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFADisabledAndConsentCheckDisabled(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: false,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(success)
         })
@@ -55,7 +32,7 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFAEnabledAndConsentCheckDisabled(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(success)
         })
@@ -64,7 +41,7 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFAEnabledAndConsentCheckEnabled(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(success)
         })
@@ -74,7 +51,7 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFADisabledAndConsentCheckWithInvalidURL(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "invalid url",shouldFetchIDFA: false,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(error != nil)
         })
@@ -83,7 +60,7 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFADisabledAndConsentCheckDisabledWithInvalidURL(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "invalid url",shouldFetchIDFA: false,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(error != nil)
         })
@@ -92,7 +69,7 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFAEnabledAndConsentCheckDisabledWithInvalidURL(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "invalid url",shouldFetchIDFA: true,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(error != nil)
         })
@@ -101,45 +78,45 @@ class EdgeTagSDKTests: XCTestCase {
     func testInitWithIDFAEnabledAndConsentCheckEnabledWithInvalidURL(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "invalid url",shouldFetchIDFA: true,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             XCTAssert(error != nil)
         })
     }
     
-func testConsent(){
-    
-    let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: false)
-    let edgeTagManager = EdgeTagManager.shared
-
-    let exp = expectation(description: "\(#function)\(#line)")
-    
-    edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
-        if success{
-        edgeTagManager.giveConsentForProviders(consent: ["facebook":false, "smart":false,"all":true], completion: { success, error in
-            XCTAssert(success)
-            exp.fulfill()
+    func testConsent(){
+        
+        let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: false)
+        let edgeTagManager = EdgeTagManager.shared
+        
+        let exp = expectation(description: "\(#function)\(#line)")
+        
+        edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
+            if success{
+                edgeTagManager.giveConsentForProviders(consent: ["facebook":false, "smart":false,"all":true], completion: { success, error in
+                    XCTAssert(success)
+                    exp.fulfill()
+                })
+            }
         })
-        }
-    })
         waitForExpectations(timeout: 40, handler: nil)
-    
-}
+        
+    }
     func testTagWithDisableConsentAndNOUserINFO(){
-
+        
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
-
+        
         let exp = expectation(description: "\(#function)\(#line)")
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.addTag(withData: ["value":"20.00","currency":"USD"], eventName: "cartEvent", providers: ["all":false,"facebook":true], completion: { success, error in
-                XCTAssert(success)
-                exp.fulfill()
-            })
+                edgeTagManager.addTag(withData: ["value":"20.00","currency":"USD"], eventName: "cartEvent", providers: ["all":false,"facebook":true], completion: { success, error in
+                    XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     func testTagWithEnableConsentAndNOUserINFO(){
@@ -150,81 +127,81 @@ func testConsent(){
         let exp = expectation(description: "\(#function)\(#line)")
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.giveConsentForProviders(consent: ["facebook":false, "smart":false,"all":true], completion: { success, error in
-                edgeTagManager.addTag(withData: ["value":"20.00","currency":"USD"], eventName: "cartEvent", providers: ["all":false,"facebook":true], completion: { success, error in
-                    XCTAssert(success)
-                    exp.fulfill()
+                edgeTagManager.giveConsentForProviders(consent: ["facebook":false, "smart":false,"all":true], completion: { success, error in
+                    edgeTagManager.addTag(withData: ["value":"20.00","currency":"USD"], eventName: "cartEvent", providers: ["all":false,"facebook":true], completion: { success, error in
+                        XCTAssert(success)
+                        exp.fulfill()
+                    })
                 })
-            })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     func testUserDataDictionaryWithDisableConsent(){
-
+        
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
         let exp = expectation(description: "\(#function)\(#line)")
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-           edgeTagManager.addDataIDGraph(idGraph: ["email":"me@abckl.ij","cutomInfo":"Random string entry"], completion: { success, error in                XCTAssert(success)
-               exp.fulfill()
-            })
+                edgeTagManager.addDataIDGraph(idGraph: ["email":"me@abckl.ij","cutomInfo":"Random string entry"], completion: { success, error in                XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     func testUserDataDictionaryWithEnableConsent(){
-
+        
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
         let exp = expectation(description: "\(#function)\(#line)")
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.addDataIDGraph(idGraph: ["email":"me@abckl.ij","cutomInfo":"Random string entry"], completion: { success, error in              XCTAssert(success)
-                exp.fulfill()
-            })
+                edgeTagManager.addDataIDGraph(idGraph: ["email":"me@abckl.ij","cutomInfo":"Random string entry"], completion: { success, error in              XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     func testUserDataWithDisableConsent(){
-
+        
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: true)
         let edgeTagManager = EdgeTagManager.shared
         let exp = expectation(description: "\(#function)\(#line)")
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.addUserIDGraph(userKey: "email", userValue: "me@domain.com", completion: { success, error in
-                XCTAssert(success)
-                exp.fulfill()
-            })
+                edgeTagManager.addUserIDGraph(userKey: "email", userValue: "me@domain.com", completion: { success, error in
+                    XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     func testUserDataWithEnableConsent(){
-
+        
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
         let exp = expectation(description: "\(#function)\(#line)")
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.addUserIDGraph(userKey: "email", userValue: "me@domain.com", completion: { success, error in
-                XCTAssert(success)
-                exp.fulfill()
-            })
+                edgeTagManager.addUserIDGraph(userKey: "email", userValue: "me@domain.com", completion: { success, error in
+                    XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     func testGetData()
@@ -232,33 +209,33 @@ func testConsent(){
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
         let exp = expectation(description: "\(#function)\(#line)")
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.getDataIDGraph(idGraphKeys: ["cutomInfo","numberValue","testBool","email","invalid value"], completion: { success, error, idGraph in
-                XCTAssert(success)
-                exp.fulfill()
-            })
+                edgeTagManager.getDataIDGraph(idGraphKeys: ["cutomInfo","numberValue","testBool","email","invalid value"], completion: { success, error, idGraph in
+                    XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
-   
+    
     func testGetKeys()
     {
         let edgeConfiguration = EdgeTagConfiguration(withUrl: "https://sdk-demo-t.edgetag.io",shouldFetchIDFA: true,disableConsentCheck: false)
         let edgeTagManager = EdgeTagManager.shared
         let exp = expectation(description: "\(#function)\(#line)")
-
+        
         edgeTagManager.initEdgeTag(withEdgeTagConfiguration: edgeConfiguration, completion: { success, error in
             if success{
-            edgeTagManager.getUserKeys(completion: { success, error, idGraphKeys in
-                XCTAssert(success)
-                exp.fulfill()
-            })
+                edgeTagManager.getUserKeys(completion: { success, error, idGraphKeys in
+                    XCTAssert(success)
+                    exp.fulfill()
+                })
             }
         })
-            waitForExpectations(timeout: 40, handler: nil)
+        waitForExpectations(timeout: 40, handler: nil)
     }
     
     
@@ -267,7 +244,7 @@ func testConsent(){
     }
     
     func testGetKVForUserData(){
-       let dict = PackageProviders.shared.getKVForUserData()
+        let dict = PackageProviders.shared.getKVForUserData()
         XCTAssert(dict.keys.count > 0)
     }
     
